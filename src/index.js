41a1e70c8ccb07b7b1d404b4878a58e047fa719e
Afeez Awoyemi <damafeez@gmail.com>
2020-08-01 14:25:23 +0100

    chore(bundle): bundle with rollup/sfc
    - new bundle method for ssr compatibility

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-import IconBase from './IconBase.vue'
+// Import vue component
+import component from '@/feather-icons-vue.vue';
 
-export default {
-  install(Vue, options = {}) {
-    Vue.component(options.as || IconBase.name, IconBase)
-  },
+// install function executed by Vue.use()
+const install = function installFeatherIconsVue(Vue, options = {}) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component(options.as || 'IconBase', component);
+};
+
+// Create module definition for Vue.use()
+const plugin = {
+  install,
+};
+
+// To auto-install on non-es builds, when vue is found
+// eslint-disable-next-line no-redeclare
+/* global window, global */
+if ('false' === process.env.ES_BUILD) {
+  let GlobalVue = null;
+  if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+  } else if (typeof global !== 'undefined') {
+    GlobalVue = global.Vue;
+  }
+  if (GlobalVue) {
+    GlobalVue.use(plugin);
+  }
 }
+
+// Inject install function into component - allows component
+// to be registered via Vue.use() as well as Vue.component()
+component.install = install;
+
+// Export component by default
+export default component;
+
+// It's possible to expose named exports when writing components that can
+// also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
+// export const RollupDemoDirective = component;
